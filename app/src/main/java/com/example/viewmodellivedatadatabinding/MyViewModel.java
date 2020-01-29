4b3711209021b2c5f947b582960258f429de0cff
sunlongyu <1223956215@qq.com>
2020-01-29 19:07:18 +0800

    第一个整合案例

---
+++ app/src/main/java/com/example/viewmodellivedatadatabinding/MyViewModel.java
@@ -0,0 +1,20 @@
+package com.example.viewmodellivedatadatabinding;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> number;
+
+    public MutableLiveData<Integer> getNumber() {
+        if (number == null) {
+            number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+        return number;
+    }
+
+    public void add(int n) {
+        number.setValue(number.getValue() + n);
+    }
+}
